c2863f1b78c7c38042c39d8cc9fd7ee3e9316378
Jesús Olmos <jesusolmossanchez@gmail.com>
2019-10-21 10:44:38 +0200

    metiendo todo el html después!

--- main.js
+++ main.js
@@ -188,6 +188,295 @@ function empieza_todo(){
         $("#fondo_negro").fadeOut(1000);
         $("#trabajos_link").addClass("activo");
 
+        var hiddenHtml = `
+        <div id="left" class="bloque_oblicuo">
+            <div class="inner_bloque_oblicuo">
+                <div class="contiene_proyecto shck">
+                    <div class="bg_proyecto"></div>
+                    <p>
+                        Jumanji Running - SONY
+                        <br/><br/>Hybrid app for iOS and Android
+                        <span class="texto_extra">
+                            <br/>'Running app' with geolocation and gamification
+                            <br/>Made with cordova using no other JS framework
+                            <br/>Agency: Shackleton
+                        </span>
+                    </p>
+                    <img loading="lazy" src="imagenes/jumanji.jpg">
+                    <div class="bloque_extra">
+                        <img loading="lazy" src="imagenes/jumanji2.jpg">
+                        <img loading="lazy" src="imagenes/jumanji3.png">
+                        <img loading="lazy" src="imagenes/jumanji4.jpg">
+                        <img loading="lazy" src="imagenes/jumanji5.jpg">
+                        <img loading="lazy" src="imagenes/jumanji6.jpg">
+                    </div>
+                </div>
+                
+                <div class="contiene_proyecto personal">
+                    <div class="bg_proyecto"></div>
+                    <p>
+                        __Off_the_line__
+                        <br/>
+                        <br/>HTML5 PWA web game
+                        <br/>
+                        <br/>TWA Android APP
+                        <span class="texto_extra">
+                            <br/>
+                            <br/>Less than 13kbs in plain javascript
+                            <br/>js13kgames Contest
+                            <br/>
+                            <a target="_blank" href="https://offtheline.jesusolmos.es/">link</a>
+                            <a target="_blank" href="https://play.google.com/store/apps/details?id=com.jesusolmos.lostangry">
+                                <img style="width: 150px; display: block; margin: 30px 0;" src="imagenes/gp_icon.png" alt="app_link">
+                            </a>
+                        </span>
+                    </p>
+                    <div class="bloque_extra">
+                        <img loading="lazy" src="imagenes/offtheline_gif1.gif">
+                        <img loading="lazy" src="imagenes/offtheline2.png">
+                        <img loading="lazy" src="imagenes/offtheline3.png">
+                        <img loading="lazy" src="imagenes/offtheline_gif2.gif">
+                    </div>
+                    <img loading="lazy" src="imagenes/offtheline1.jpg">
+                </div>
+                <div class="contiene_proyecto personal con_video">
+                    <div class="bg_proyecto"></div>
+                    <p>
+                        Lost and Angry
+                        <br/>
+                        <br/>HTML5 PWA web game
+                        <br/>
+                        <br/>TWA Android APP
+                        <span class="texto_extra">
+                            <br/>
+                            <br/>Less than 13kbs in plain javascript
+                            <br/>js13kgames Contest
+                            <br/>
+                            <a href="https://lost-angry.jesusolmos.es/" target="_blank">link</a>
+                            <a href="https://play.google.com/store/apps/details?id=com.jesusolmos.lostangry" target="_blank">
+                                <img style="width: 150px; display: block; margin: 30px 0;" src="imagenes/gp_icon.png" alt="app_link">
+                            </a>
+                        </span>
+                    </p>
+                    <img loading="lazy" src="imagenes/lost_angry.jpg">
+                    <div class="contiene_video">
+                        <video loop>
+                            <source src="videos/lost_and_angry.mp4" type="video/mp4">
+                        </video>
+                    </div>
+                </div>
+                <div class="contiene_proyecto shck con_video">
+                    <div class="bg_proyecto"></div>
+                    <p>
+                        Superpoderes - ABANCA
+                        <br/>
+                        <br/>Corporative webpage
+                        <span class="texto_extra">
+                            <br/>LAMP enviroment with CSS3 animations
+                            <br/>Agency: Shackleton
+                        </span>
+                    </p>
+                    <img loading="lazy" src="imagenes/superpoderes.jpg">
+                    <div class="contiene_video">
+                        <video loop>
+                            <source src="videos/abanca_super.mp4" type="video/mp4">
+                        </video>
+                    </div>
+                    <div class="bloque_extra">
+                        <img loading="lazy" src="imagenes/superpoderes2.jpg">
+                    </div>
+                </div>
+                
+                <div class="contiene_proyecto shck" style="background: #000;">
+                    <div class="bg_proyecto"></div>
+                    <p>
+                        Sound and Grow - HEINZ
+                        <br/><br/>Hybrid app for iOS and Android
+                        <span class="texto_extra">
+                            <br/>'Educational app' With geolocation and wheater prediction
+                            <br/>Made with cordova using onsen framework
+                            <br/>MySQL and PHP backend
+                            <br/>Agency: Shackleton
+                        </span>
+                    </p>
+                    <img loading="lazy" src="imagenes/sound_grow.jpg">
+                    <div class="bloque_extra">
+                        <img loading="lazy" src="imagenes/sound_grow2.jpg">
+                        <iframe src="https://www.youtube.com/embed/4gDz4QFOPrI?rel=0" frameborder="0" allowfullscreen=""></iframe>
+                        <img loading="lazy" src="imagenes/sound_grow3.jpg">
+                    </div>
+                </div>
+                <div class="contiene_proyecto okn" style="background: #000;">
+                    <div class="bg_proyecto"></div>
+                    <p>
+                        OKN learning
+                        <br/><br/>E-learning environment for business
+                        <span class="texto_extra">
+                            <br/>Made with PHP over Azure distributed architecture
+                            <br/>MariaDB database with Propel ORM
+                            <br/>Being part of a great team of developers
+                        </span>
+                    </p>
+                    <img loading="lazy" src="imagenes/okn1.png">
+                    <div class="bloque_extra">
+                        <img loading="lazy" src="imagenes/okn2.png">
+                        <img loading="lazy" src="imagenes/okn3.png">
+                        <img loading="lazy" src="imagenes/okn4.png">
+                        <img loading="lazy" src="imagenes/okn5.png">
+                        <img loading="lazy" src="imagenes/okn6.png">
+                    </div>
+                </div>
+                
+                <div class="contiene_proyecto personal con_video">
+                    <div class="bg_proyecto"></div>
+                    <p>
+                        Dudevolley
+                        <br/><br/>HTML5 web game
+                        <span class="texto_extra">
+                            <br/>Made with PhaserJS framework
+                            <br/>NodeJS and MongoDB backend
+                            <br/>Implements multiplayer mode through websocket
+                            <br/><a target="_blank" href="https://www.dudevolley.com">www.dudevolley.com</a> 
+                        </span>
+                    </p>
+                    <img loading="lazy" src="imagenes/dudevolley.png">
+                    <div class="contiene_video">
+                        <video loop>
+                            <source src="videos/dude.mp4" type="video/mp4">
+                        </video>
+                    </div>
+                </div>
+                <div class="contiene_proyecto personal">
+                    <div class="bg_proyecto"></div>
+                    <p>
+                        Sockies
+                        <br/><br/>E-commerce web made with Prestashop
+                        <span class="texto_extra">
+                            <br/>www.sockies.es
+                        </span>
+                    </p>
+                    <img loading="lazy" src="imagenes/sockies.jpg">
+                </div>
+                <div class="contiene_proyecto personal">
+                    <div class="bg_proyecto"></div>
+                    <p>
+                        Cyprea
+                        <br/><br/>E-commerce web made with Prestashop
+                        <span class="texto_extra">
+                            <br/>www.cyprea.es
+                        </span>
+                    </p>
+                    <img loading="lazy" src="imagenes/cyprea.jpg">
+                </div>
+                <div class="contiene_proyecto personal con_video" style="background:#cecece">
+                    <div class="bg_proyecto"></div>
+                    <p>
+                        DIKTIA
+                        <br/><br/>Corporative webpage (under construction)
+                        <span class="texto_extra">
+                            <br/><a target="_blank" href="http://www.diktia.com">www.diktia.com</a> 
+                        </span>
+                    </p>
+                    <img loading="lazy" src="imagenes/32.png">
+                    <div class="contiene_video">
+                        <video loop>
+                            <source src="videos/diktia.mp4" type="video/mp4">
+                        </video>
+                    </div>
+                </div>
+                <div class="contiene_proyecto shck con_video">
+                    <div class="bg_proyecto"></div>
+                    <p>
+                        Piensa sin prejuicios - P.P.
+                        <br/><br/>HTML5 quiz game
+                        <span class="texto_extra">
+                            <br/>Made in plain JS
+                            <br/>Agency: Shackleton
+                        </span>
+                    </p>
+                    <img loading="lazy" src="imagenes/piensa_sin_prejuicios.jpg">
+                    <div class="contiene_video">
+                        <video loop>
+                            <source src="videos/pp_game.mp4" type="video/mp4">
+                        </video>
+                    </div>
+                </div>
+                <div class="contiene_proyecto shck" style="display:none">
+                    <div class="bg_proyecto"></div>
+                    <p>
+                        Desarrollo Web
+                        <br/>Campaña ACH
+                        <br/>Shot in 2016
+                        <br/>Agency: Shackleton
+                    </p>
+                    <img loading="lazy" src="imagenes/shotin20016.jpg">
+                </div>
+                <div class="contiene_proyecto shck con_video">
+                    <div class="bg_proyecto"></div>
+                    <p>
+                        Monsters Off
+                        <br/><br/>Corporative webpage
+                        <span class="texto_extra">
+                            <br/>LAMP enviroment
+                            <br/>Agency: Shackleton
+                        </span>
+                    </p>
+                    <img loading="lazy" src="imagenes/monster_off.png">
+                    <div class="contiene_video">
+                        <video loop>
+                            <source src="videos/monster_off.mp4" type="video/mp4">
+                        </video>
+                    </div>
+                </div>
+                <div class="contiene_proyecto shck con_video">
+                    <div class="bg_proyecto"></div>
+                    <p>
+                        Qué hacen los gastos - ABANCA
+                        <br/><br/>Corporative webpage
+                        <span class="texto_extra">
+                            <br/>LAMP enviroment with CSS3 animations
+                            <br/>Agency: Shackleton
+                        </span>
+                    </p>
+                    <img loading="lazy" src="imagenes/los_gastos.jpg">
+                    <div class="contiene_video">
+                        <video loop>
+                            <source src="videos/gastos.mp4" type="video/mp4">
+                        </video>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="right"  class="bloque_oblicuo">
+            <div class="inner_bloque_oblicuo">
+                <br/>
+                <br/>
+                I'm a web developer based in Spain.<br/> <br/> 
+                I love creating innovative experiences<br/> 
+                for the web, using every time the tools I need.<br/>
+                Javascript, CSS, PHP, NodeJS, MySQL, MongoDB...
+                <br/>
+                <br/>
+                Social Fingerprint:
+                <br/>
+                <br/>
+                <div id="sociales">
+                    <a href="https://es.linkedin.com/in/jesusolmossanchez" target="_blank">
+                        <img loading="lazy" src="imagenes/linkedin.png">
+                    </a>
+                    <a href="https://stackoverflow.com/users/5506301/jolmos" target="_blank">
+                        <img loading="lazy" src="imagenes/stackoverflow.png">
+                    </a>
+                    <a href="https://github.com/jesusolmossanchez" target="_blank">
+                        <img loading="lazy" src="imagenes/github.png">
+                    </a>
+                </div>
+                <br/>
+                <a href="mailto:jesusolmossanchez@gmail.com">Say Hello!</a>
+            </div>
+        </div>`;
+
+        $("#trabajos").append(hiddenHtml);
     }, 4500);
 
 
